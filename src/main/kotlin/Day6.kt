fdb1cfd55f4633f31c4d876efaf7b6bba4aa4d92
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-12-06 13:39:43 +0100

    Day6 Solved

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,82 @@
+import java.awt.Event.DOWN
+import java.awt.Event.UP
+import java.io.File
+
+fun day6(){
+    val file = File("src/main/resources/Day6Input.txt")
+
+    val rawDataInput = file.readLines()
+
+    val outputMap = mutableMapOf<Pair<Int,Int>,Boolean>()
+
+    val guardStart = rawDataInput.withIndex().firstNotNullOf{ (i,it) ->
+        val j =it.indexOf("^")
+        if(j == -1) null else i to j
+    }
+
+    var guardPosition = guardStart
+    var direction = Direction.UP
+
+    while (rawDataInput.getField(guardPosition) != null){
+        val look = when(direction){
+            Direction.UP -> guardPosition addX -1
+            Direction.DOWN -> guardPosition addX 1
+            Direction.RIGHT -> guardPosition addY 1
+            Direction.LEFT -> guardPosition addY -1
+        }
+        if(rawDataInput.getField(look) == '#'){
+            direction = direction.rightTurn
+            continue
+        }
+        guardPosition = look
+        outputMap[look] = true
+    }
+
+    println(outputMap.size)
+
+    outputMap.remove(guardStart)
+    val loopObstacles = outputMap.count { (obstacle,_) ->
+        val runMap = mutableMapOf<Pair<Int,Int>,List<Direction>>()
+        guardPosition = guardStart
+        direction = Direction.UP
+        while (rawDataInput.getField(guardPosition) != null && runMap[guardPosition]?.contains(direction) != true){
+            runMap.merge(guardPosition,listOf(direction)){a,b -> a+b}
+            val look = when(direction){
+                Direction.UP -> guardPosition addX -1
+                Direction.DOWN -> guardPosition addX 1
+                Direction.RIGHT -> guardPosition addY 1
+                Direction.LEFT -> guardPosition addY -1
+            }
+            if(look == obstacle ||rawDataInput.getField(look) == '#'){
+                direction = direction.rightTurn
+                continue
+            }
+            guardPosition = look
+        }
+        runMap[guardPosition]?.contains(direction) ?: false
+    }
+
+    println(loopObstacles)
+}
+
+private fun List<String>.getField(pos : Pair<Int,Int>) = getOrNull(pos.first)?.getOrNull(pos.second)
+
+private infix fun Pair<Int,Int>.addX(x : Int) = (first + x) to second
+private infix fun Pair<Int,Int>.addY(y : Int) = first to (second + y)
+
+private enum class Direction {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT;
+
+    val rightTurn by lazy {
+        when (this) {
+            UP -> RIGHT
+            RIGHT -> DOWN
+            DOWN -> LEFT
+            LEFT -> UP
+        }
+    }
+
+}

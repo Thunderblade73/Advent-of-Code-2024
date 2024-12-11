981dd3b72faa6ed71aaf112b2bfaba8babcf5e2d
Thunderblade73 <gaidermarkus@gmail.com>
2024-12-11 17:45:15 +0100

    Day11 update

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,13 +1,9 @@
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.util.*
 import java.util.concurrent.ConcurrentLinkedDeque
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
-import java.util.concurrent.ThreadPoolExecutor
 import kotlin.coroutines.*
 import kotlin.time.Duration.Companion.seconds
 
@@ -18,50 +14,82 @@ suspend fun day11(){
 
     var firstRun = inState
     for( x in 0..24){
-        firstRun = firstRun.permute()
+        firstRun = firstRun.permuteList()
     }
 
     println(firstRun.size)
     println()
 
-    val workStack = ConcurrentLinkedDeque<Triple<Int,Int,List<Long>>>()
-    val resultStack = mutableListOf<Triple<Int,Int,List<Long>>>()
+//    val workStack = ConcurrentLinkedDeque<Triple<Int,Int,List<Long>>>()
+//    val resultStack = mutableListOf<Triple<Int,Int,List<Long>>>()
+//
+//    workStack.addAll(firstRun.mapIndexed{i,it -> Triple(i,24,listOf(it))})
+//
+//    fun doWork(it : Triple<Int,Int,List<Long>>){
+//        val a = Triple(it.first,it.second+1,it.third.permute())
+//        println("${it.second} : ${it.first}")
+//        if(it.second == 73){
+//            resultStack.add(a)
+//        }else{
+//            workStack.add(a)
+//        }
+//    }
+//
+//    runBlocking {
+//        for (i in 0..63){
+//            launch {
+//                while (workStack.isNotEmpty()){
+//                    val a = workStack.poll() ?: break
+//                    doWork(a)
+//                }
+//            }
+//        }
+//    }
+//
+//    while (workStack.isNotEmpty()){
+//        delay(10.seconds)
+//    }
+//    println(resultStack.sumOf { it.third.size })
 
-    workStack.addAll(firstRun.mapIndexed{i,it -> Triple(i,24,listOf(it))})
+    //println(inState.map { it.permute(0,75) }.flatten().size)
 
-    fun doWork(it : Triple<Int,Int,List<Long>>){
-        val a = Triple(it.first,it.second+1,it.third.permute())
-        println("${it.second} : ${it.first}")
-        if(it.second == 73){
-            resultStack.add(a)
-        }else{
-            workStack.add(a)
-        }
-    }
+    1L.permute(0,5)
+}
 
-    runBlocking {
-        for (i in 0..63){
-            launch {
-                while (workStack.isNotEmpty()){
-                    val a = workStack.poll() ?: break
-                    doWork(a)
-                }
-            }
-        }
-    }
+private val memo = mutableMapOf<Long,Map<Int,List<Long>>>()
 
-    while (workStack.isNotEmpty()){
-        delay(10.seconds)
-    }
+fun Long.calculatePermute() : List<Long>{
+    if(this == 0L) return listOf(1L)
+    if(this.toString().length % 2 == 0) return this.split()
 
-
-    println(resultStack.sumOf { it.third.size })
+    return listOf(this * 2024L)
 }
 
 
+fun Long.permute(iteration : Int,target: Int) : List<Long>{
+    var memoVal = memo[this] ?: emptyMap()
+    val skip = memoVal[target-iteration-1]
+    if(skip != null) return skip
 
-fun List<Long>.permute() = buildList<Long>{
-    val (zero,zeroRemain) = partition { it == 0L }
+
+
+
+    /*
+    1
+
+    0 2024
+    1 20 24
+    2 2 0 2 4
+    3 4048 1 4048 8096
+    4 40 48 2024 40 48 80 96
+
+
+     */
+
+}
+
+fun List<Long>.permuteList() = buildList<Long>{
+    val (zero,zeroRemain) = this@permuteList.partition { it == 0L }
 
     // Zero Rule
     addAll(zero.map { 1L })
